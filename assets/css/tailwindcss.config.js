a66d74ba5505cb1c38f348dd6861dd5185305e63
budparr <budparr@gmail.com>
2020-01-29 15:37:15 -0500

    Move to folder

---
+++ assets/css/tailwindcss.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Mark",
+          "Roboto",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ]
+      },
+      opacity: {
+        "70": "0.7"
+      },
+      width: {
+        "60%": "60%;"
+      }
+    },
+    container: {
+      center: true,
+      padding: "1rem"
+    }
+  }
+};
