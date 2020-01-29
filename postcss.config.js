32d3792985b52e16c377ca6b3f6233032ddc6e6e
budparr <budparr@gmail.com>
2020-01-29 15:36:31 -0500

    Clean up PostCSS file

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,27 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./layouts/**/*.html", "./assets/js/**/*.{js,vue}"],
+  extractors: [
+    {
+      extractor: class {
+        static extract(content) {
+          //console.log(process.env.NODE_ENV);
+          return content.match(/[A-z0-9-:\/]+/g);
+          //return content.match(/[A-z0-9-:\/]+/g) || [];
+        }
+      },
+      extensions: ["vue", "html", "js"]
+    }
+  ],
+  whitelist: []
+});
+
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      path: ["assets/"],
-    }), 
-    require('tailwindcss')("./assets/tailwindcss.config.js"),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV !== "development"
-    ? [
-        require("@fullhuman/postcss-purgecss")({
-          content: ["./layouts/**/*.html", "./assets/js/**/*.{js,vue}"],
-          extractors: [
-            {
-              extractor: class {
-                static extract(content) {
-                  //console.log(process.env.NODE_ENV);
-                  return content.match(/[A-z0-9-:\/]+/g);
-                  //return content.match(/[A-z0-9-:\/]+/g) || [];
-                }
-              },
-              extensions: ["vue", "html", "js"]
-
-            }
-          ],
-          whitelist: [
-          ]
-        })
-      ]
-    : []) //If Development, do not use PurgeCSS
+    require("postcss-import")({
+      path: ["assets/css"]
+    }),
+    require("tailwindcss")("./assets/css/tailwindcss.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV !== "development" ? [purgecss] : [])
   ]
-}
+};
