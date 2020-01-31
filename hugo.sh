5159ad589cf28c8d984e51431399b8f75f1ea223
budparr <budparr@gmail.com>
2020-01-31 13:28:28 -0500

    Add Bash script to use local Hugo

---
+++ hugo.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+#Required to be SET. TODO: Test for this, and if version is not present load from present. Use something like https://zwbetz.com/script-to-install-latest-hugo-release-on-linux-and-mac/
+HUGO_VERSION=$(node -p "require('./package.json').hugo.version")
+HUGO_TYPE=$(node -p "require('./package.json').hugo.extended")
+
+set -e
+
+if [ -n $HUGO_VERSION ]; then
+    echo Installing $HUGO_VERSION
+else
+    exit
+fi
+
+#
+if [ $HUGO_TYPE != false ] || [ $HUGO_TYPE != "false" ]; then
+    echo WE ARE Using Hugo Extended
+    EXTENDED="extended_"
+else
+    echo NOT Using Hugo Extended
+    EXTENDED=""
+fi
+
+echo Cleaning bin directory
+
+if [ -d ${PWD}/bin ]; then
+    rm -rf ${PWD}/bin
+fi
+
+echo Make temp directory
+
+mkdir -p ${PWD}/temp ${PWD}/bin
+
+echo Move to temp directory
+
+pushd ${PWD}/temp
+
+echo Current Directory: ${PWD}
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${EXTENDED}${HUGO_VERSION}_macOS-64bit.tar.gz -o hugo.tar.gz
+
+TARBALL=hugo.tar.gz
+
+echo Extracting $TARBALL
+
+tar --extract --file $TARBALL
+
+# do we need sudo command for these?
+chmod +x hugo
+
+mv hugo $DIR/bin
+
+# Clean up temp directory (popd) ?
+rm -rf $DIR/temp
+
+location="$(which $DIR/bin/hugo)"
+echo "Hugo binary location: $location"
+
+version="$($DIR/bin/hugo version)"
+echo $version >$DIR/bin/version.md
