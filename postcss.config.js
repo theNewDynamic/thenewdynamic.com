5fe21f7f94b2b92376a3fe96eaf34ee9efd5d984
budparr <budparr@gmail.com>
2020-02-05 12:01:43 -0500

    Simplify PurgeCSS extractor

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./layouts/**/*.html", "./assets/js/**/*.{js,vue}"],
-  extractors: [
-    {
-      extractor: class {
-        static extract(content) {
-          //console.log(process.env.NODE_ENV);
-          return content.match(/[A-z0-9-:\/]+/g);
-          //return content.match(/[A-z0-9-:\/]+/g) || [];
-        }
-      },
-      extensions: ["vue", "html", "js"]
-    }
-  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
   whitelist: []
 });
 
