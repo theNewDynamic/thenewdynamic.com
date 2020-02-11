73d47e89fb2ab0e199222fcd4abd9b8b6d703dec
budparr <budparr@gmail.com>
2020-02-10 22:45:09 -0500

    [wip] add some styling to article pages

--- assets/css/tailwindcss.config.js
+++ assets/css/tailwindcss.config.js
@@ -1,34 +1,58 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Mark",
-          "Roboto",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          '"Helvetica Neue"',
-          "Arial",
-          '"Noto Sans"',
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"'
-        ]
-      },
-      opacity: {
-        "70": "0.7"
-      },
-      width: {
-        "60%": "60%;"
-      }
-    },
-    container: {
-      center: true,
-      padding: "1rem"
-    }
-  }
+	theme: {
+		extend: {
+			colors: {
+				notwhite: {
+					100: "#FEFEFF",
+					200: "#FDFEFE",
+					300: "#FBFDFD",
+					400: "#F9FBFC",
+					500: "#F6F9FB",
+					600: "#DDE0E2",
+					700: "#949597",
+					800: "#6F7071",
+					900: "#4A4B4B"
+        },
+        'primary': {
+          100: '#F6E7EB',
+          200: '#E8C2CD',
+          300: '#DA9DAE',
+          400: '#BF5472',
+          500: '#A30B35',
+          600: '#930A30',
+          700: '#620720',
+          800: '#490518',
+          900: '#310310',
+          },
+			},
+			fontFamily: {
+				sans: [
+					"Mark",
+					"Roboto",
+					"-apple-system",
+					"BlinkMacSystemFont",
+					'"Segoe UI"',
+					"Roboto",
+					'"Helvetica Neue"',
+					"Arial",
+					'"Noto Sans"',
+					"sans-serif",
+					'"Apple Color Emoji"',
+					'"Segoe UI Emoji"',
+					'"Segoe UI Symbol"',
+					'"Noto Color Emoji"'
+				]
+			},
+			opacity: {
+				"70": "0.7"
+			},
+			width: {
+				"60%": "60%;"
+			}
+		},
+		container: {
+			center: true,
+			padding: "1rem"
+		}
+	}
 };
