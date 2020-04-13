0db7e6c6c05eeb8a645fd3f465c04895fd920ce9
Regis Philibert <login@regisphilibert.com>
2020-04-13 17:17:38 -0400

    Revert to using good old way identifying purgeable classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./layouts/**/*.html", "./assets/js/**/*.{js,vue}"],
+  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+  whitelist: []
+});
+/* Currenlty, as of Hugo .69.0, the some classed are not picked up and added to the json file.
+Once it's fixed we'll be able to use it.
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
@@ -5,7 +12,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
       return els.tags.concat(els.classes, els.ids);
   }
 });
-
+*/
 module.exports = {
   plugins: [
     require("postcss-import")({
