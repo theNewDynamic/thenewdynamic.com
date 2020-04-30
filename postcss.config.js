b9060b63bb2156077f33226988b7af25286d3978
budparr <budparr@gmail.com>
2020-04-30 19:46:23 -0400

    Set up asset pipeline to use Postprocess

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./layouts/**/*.html", "./assets/js/**/*.{js,vue}"],
-  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-  whitelist: []
+	content: ["./hugo_stats.json"],
+	defaultExtractor: (content) => {
+		let els = JSON.parse(content).htmlElements;
+		return els.tags.concat(els.classes, els.ids);
+	},
 });
-
 module.exports = {
-  plugins: [
-    require("postcss-import")({
-      path: ["assets/css"]
-    }),
-    require("tailwindcss")("./assets/css/tailwindcss.config.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV !== "development" ? [purgecss] : [])
-  ]
+	plugins: [
+		require("postcss-import")({
+			path: ["assets/css"],
+		}),
+		require("tailwindcss")("./assets/css/tailwind.config.js"),
+		require("autoprefixer"),
+		...(process.env.HUGO_ENV !== "development" ? [purgecss] : []),
+	],
 };
