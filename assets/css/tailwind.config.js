602142693386b210611802a3b147abf3e022e38c
budparr <budparr@gmail.com>
2020-05-25 12:41:47 -0400

    Add typography settings

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,95 @@
-const colors = require('./colors.json')
+const colors = require("./colors.json");
 
 module.exports = {
 	theme: {
+		textStyles: (theme) => ({
+			heading: {
+				output: false,
+				fontWeight: theme("fontWeight.bold"),
+				lineHeight: theme("lineHeight.tight"),
+			},
+			h1: {
+				extends: "heading",
+				fontSize: theme("fontSize.5xl"),
+				"@screen sm": {
+					fontSize: theme("fontSize.6xl"),
+				},
+			},
+			h2: {
+				extends: "heading",
+				fontSize: theme("fontSize.4xl"),
+				"@screen sm": {
+					fontSize: theme("fontSize.5xl"),
+				},
+			},
+			h3: {
+				extends: "heading",
+				fontSize: theme("fontSize.4xl"),
+			},
+			h4: {
+				extends: "heading",
+				fontSize: theme("fontSize.3xl"),
+			},
+			h5: {
+				extends: "heading",
+				fontSize: theme("fontSize.2xl"),
+			},
+			h6: {
+				extends: "heading",
+				fontSize: theme("fontSize.xl"),
+			},
+			link: {
+				fontWeight: theme("fontWeight.bold"),
+				color: theme("colors.blue.400"),
+				"&:hover": {
+					color: theme("colors.blue.600"),
+					textDecoration: "underline",
+				},
+			},
+			richText: {
+				fontWeight: theme("fontWeight.normal"),
+				fontSize: theme("fontSize.base"),
+				lineHeight: theme("lineHeight.relaxed"),
+				"> * + *": {
+					marginTop: "1em",
+					color: theme("colors.gray.700"),
+				},
+				h1: {
+					extends: "h1",
+				},
+				h2: {
+					extends: "h2",
+				},
+				h3: {
+					extends: "h3",
+				},
+				h4: {
+					extends: "h4",
+				},
+				h5: {
+					extends: "h5",
+				},
+				h6: {
+					extends: "h6",
+				},
+				ul: {
+					marginLeft: "2rem",
+					listStyleType: "disc",
+				},
+				ol: {
+					listStyleType: "decimal",
+				},
+				a: {
+					extends: "link",
+				},
+				"b, strong": {
+					fontWeight: theme("fontWeight.bold"),
+				},
+				"i, em": {
+					fontStyle: "italic",
+				},
+			},
+		}),
 		extend: {
 			colors,
 			minHeight: {
@@ -40,5 +128,10 @@ module.exports = {
 			padding: "1rem",
 		},
 	},
-	plugins: [require("@tailwindcss/ui")],
+	plugins: [
+		require("@tailwindcss/ui"),
+		require("tailwindcss-typography")({			
+			componentPrefix: "", // the prefix to use for text style classes
+		}),
+	],
 };
