c8911e2692ddd3d6aa16c6da77f3f697c2983a93
budparr <budparr@gmail.com>
2020-05-28 11:28:18 -0400

    Imporve my intersection observer code to be more reusable

--- assets/js/intersection-observer.js
+++ assets/js/intersection-observer.js
@@ -1,15 +1,17 @@
-const heading = document.getElementById("pageheading");
+const pageHeading = document.querySelectorAll("#pageheading");
 
 let observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
-        // The target is the div AFTER the heading. 
-		const target = entry.target.nextElementSibling;			
+		// The target is the div AFTER the heading.
+		const target = entry.target.nextElementSibling;
 		if (entry.intersectionRatio > 0) {
-			target.classList.add('opacity-0');			
+			target.classList.add("opacity-0");
 		} else {
-			target.classList.remove("opacity-0");			
+			target.classList.remove("opacity-0");
 		}
 	});
 });
 
-observer.observe(heading);
+pageHeading.forEach((heading) => {
+	observer.observe(heading);
+});
