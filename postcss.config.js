195f6da47b6c8ef09ea5c3241ab13b762d2eb26b
Regis Philibert <login@regisphilibert.com>
2020-08-27 14:07:37 -0400

    Use js.Build for front scripts (#151)
    * Use js.Build for front scripts
    
    * Update Netlify hugo version
    
    * Cleaning

--- postcss.config.js
+++ postcss.config.js
@@ -14,16 +14,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 		"active",
 	],
 });
-/* Currenlty, as of Hugo .69.0, the some classed are not picked up and added to the json file.
-Once it's fixed we'll be able to use it.
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [ './hugo_stats.json' ],
-  defaultExtractor: (content) => {
-      let els = JSON.parse(content).htmlElements;
-      return els.tags.concat(els.classes, els.ids);
-  }
-});
-*/
+
 module.exports = {
 	plugins: [
 		require("postcss-import")({
