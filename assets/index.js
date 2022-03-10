0310e434cca1b0f50e45b33d50ffce67ab9ca81a
Regis Philibert <login@regisphilibert.com>
2022-03-10 11:42:11 -0500

    Move alpine data in bundle to avoid have to trust js unsafe inline

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,6 @@ import "./js/lazysizes.js";
 import "./js/quicklink.js";
 import "./js/intersection-observer.js";
 import "./js/prev-next.js";
-import "alpinejs";
 import "./js/print.js";
 import "./js/nojs.js";
+import "./js/alpine"
---
+++ assets/js/alpine/index.js
@@ -0,0 +1,6 @@
+import Alpine from `alpinejs`
+import formSupport from './form-support.js'
+ 
+Alpine.data('formSupport', formSupport)
+ 
+Alpine.start()
